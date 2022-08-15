f71c00643eb5994e4d6b10fb4654b5e63444ad21
JoKyungseo <eksql0645@gmail.com>
2022-08-15 21:03:01 +0900

    [Feat] 에러 미들웨어 response 추가

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ app.use('/recruitment', recruitmentRouter);
 // 에러 미들웨어
 app.use((err, req, res, next) => {
   console.log('\x1b[33m%s\x1b[0m', err.stack);
+  res.status(400).json({ result: 'error', reason: err.message });
 });
 
 module.exports = app;
