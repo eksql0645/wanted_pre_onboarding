9d1fd47351446c5b1ca289667f4180a2778690da
JoKyungseo <eksql0645@gmail.com>
2022-08-16 16:27:25 +0900

    [Fix] 채용공고목록 조회 시 회사명, 국가, 지역 나오도록 수정
    include와 attributes 속성을 사용해서 Company 모델 데이터 가져오고 필요한 속성만 추출

--- src/routers/recruitment-router.js
+++ src/routers/recruitment-router.js
@@ -47,22 +47,39 @@ recruitmentRouter.post('/', async (req, res, next) => {
 // 채용공고 목록 조회
 recruitmentRouter.get('/', async (req, res, next) => {
   try {
-    const recruitmentList = await Recruitment.findAll({});
+    // 회사ID로 회사명, 국가, 지역 가져오기
+    const recruitmentsList = await Recruitment.findAll({
+      include: [
+        { model: Company, attributes: ['company_name', 'nation', 'city'] },
+      ],
+      raw: true,
+      attributes: ['id', 'position', 'compensation', 'stack'],
+    });
 
     // 채용공고가 없는 경우
-    if (recruitmentList.length === 0) {
+    if (recruitmentsList.length === 0) {
       throw new Error('채용 공고가 존재하지 않습니다.');
     }
 
-    res.status(200).json(recruitmentList);
+    res.status(200).json(recruitmentsList);
   } catch (e) {
     next(e);
   }
 });
 
-// 채용공고 상세페이지
-recruitmentRouter.get('/', async (req, res, next) => {
+// 채용공고 상세페이지 / 회사명, 국가, 지역, 채용내용, 회사가 올린 다른 채용공고
+recruitmentRouter.get('/:id', async (req, res, next) => {
   try {
+    const { id } = req.params;
+
+    // 채용공고가 존재하는지 확인
+    const recruitment = await Recruitment.findOne({ id });
+
+    if (!recruitment) {
+      throw new Error('해당 채용공고가 존재하지 않습니다.');
+    }
+
+    res.status(200).json(recruitment);
   } catch (e) {
     next(e);
   }
